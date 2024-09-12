bdd24191d0db44a36c4f16d025cffe2465024683
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2024-09-12 12:07:02 -0400

    Time: 0 ms (100.00%) | Memory: 7.7 MB (95.45%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     bool isValid(string s) {
+        // exit if number of characters is odd
+        if(s.size() % 2 != 0) return false;
+
         stack<char> para;
+
         for(char c: s){
             if(c == '(' || c == '{' || c == '[') para.push(c);
             else 
             {
-                if(para.empty()) return false; // if first char is closing bracket, then parantheses are not valid
-                
-                if(c == ')' && para.top() != '(' || c == '}' && para.top() != '{' || c == ']' && para.top() != '[' )
+                // check if stack is empty -if first char is closing bracket, then parantheses are not valid
+                if(para.empty() || c == ')' && para.top() != '(' 
+                    || c == '}' && para.top() != '{' 
+                    || c == ']' && para.top() != '[' ) 
                     return false;
 
                 para.pop();
