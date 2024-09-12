7aa88b7b551c471bfa612fdc22aa8a4497094811
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2024-09-12 11:55:07 -0400

    Time: 4 ms (11.93%) | Memory: 7.9 MB (58.46%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
+        stack<char> para;
         for(char c: s){
-            if(c=='('|| c=='['||c=='{')
-                st.push(c);
-            else{
-                if(st.empty()) return false;
-                char top = st.top();
-                if(c==')' && top !='(' || c==']' && top != '[' || c =='}' && top !='{') return false;
-                st.pop();
-            }
+            if(c == '(' || c == '{' || c == '[') para.push(c);
+            else 
+            {
+                if(para.empty()) return false; // if first char is closing bracket, then parantheses are not valid
+                
+                if(c == ')' && para.top() != '(' || c == '}' && para.top() != '{' || c == ']' && para.top() != '[' )
+                    return false;
 
+                para.pop();
+            }
         }
-        return st.empty();
+
+        return para.empty();
     }
 };
