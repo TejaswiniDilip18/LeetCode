85c792a1bd966051353ff0ed6d8dec2def2bb952
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2024-09-19 00:41:53 -0400

    Time: 123 ms (45.93%) | Memory: 105.7 MB (52.28%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -5,7 +5,8 @@ public:
         stack<int> temp;
         for(int i=0; i<temperatures.size(); i++){
             while(!temp.empty() && temperatures[i] > temperatures[temp.top()]){
-                answer[temp.top()] = i - temp.top();
+                int prev = temp.top();
+                answer[prev] = i - prev;
                 temp.pop();
             }
 
