529d628187fb8d2bb84f453035fd4d23a1c7fb88
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2024-10-06 16:07:36 -0400

    Time: 60 ms (70.34%) | Memory: 27.5 MB (52.19%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> triplet;
+        sort(nums.begin(), nums.end());
 
-        sort(nums.begin(), nums.end());        
-        vector<vector<int>> result;
+        int size = nums.size();
 
-        for(int i = 0; i<size(nums); i++){
-
-            if(i>0 && nums[i]==nums[i-1]) continue;
-
-            int l = i+1;
-            int r = nums.size()-1;
+        for(int i=0; i<nums.size(); i++){
             
+            //skip duplicates
+            if(i>0 && nums[i] == nums[i-1]) continue; 
+
+            //initialize left and right pointers
+            int l = i+1;
+            int r = size - 1;
             while(l<r){
                 int sum = nums[i] + nums[l] + nums[r];
 
                 if(sum == 0){
-                    result.push_back({nums[i], nums[l], nums[r]});
+                    triplet.push_back({nums[i], nums[l] ,nums[r]});
                     while(l<r && nums[l]==nums[l+1]) l++;
-                    while(l<r && nums[r]== nums[r-1]) r--;
+                    while(l<r && nums[r]==nums[r-1]) r--;
+
                     l++;
                     r--;
                 }
                 else if(sum > 0) r--;
                 else l++;
             }
-        }    
-
-        return result;    
+        }
+        return triplet;
     }
 };
