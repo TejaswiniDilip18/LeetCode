62437c497a763e24fdda61f537a5895d27357314
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2024-10-18 14:47:40 -0400

    Time: 4 ms (99.99%) | Memory: 22.9 MB (75.03%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -11,11 +11,8 @@ public:
         result.push_back(intervals[0]);
 
         for(int i=0; i< intervals.size()-1; i++){
-            if(result[result.size()-1][1] >= intervals[i+1][0] && result[result.size()-1][1] <= intervals[i+1][1]){
-                result[result.size()-1][1] = intervals[i+1][1];
-            }
-            else if(result[result.size()-1][0] <= intervals[i+1][0] && result[result.size()-1][1] >= intervals[i+1][1]){
-                continue;
+            if(result[result.size()-1][1] >= intervals[i+1][0] ){
+                result[result.size()-1][1] = max(result[result.size()-1][1],intervals[i+1][1]);
             }
             else{
                 result.push_back({intervals[i+1][0], intervals[i+1][1]});
