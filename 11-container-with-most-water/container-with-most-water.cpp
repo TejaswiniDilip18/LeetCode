32bbfdbf73939b145b5a6b6ada54217cf1073907
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2024-10-22 16:13:49 -0400

    Time: 2 ms (96.80%) | Memory: 61.7 MB (74.41%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -5,7 +5,7 @@ public:
         int left = 0;
         int right = height.size() - 1;
 
-        while(left != right){
+        while(left < right){
             int currArea = (right-left)*min(height[left], height[right]);
             area = max(area, currArea);
             if(height[left] < height[right]) left ++;
