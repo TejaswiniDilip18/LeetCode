be19a51be8e2188ba7e2a253a0c544bccddf8e8a
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-01-06 18:02:52 -0500

    Time: 3 ms (90.62%) | Memory: 23.9 MB (47.82%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -4,22 +4,17 @@ public:
         if (intervals.empty()) {
         return {};
         }
-    
+        
         sort(intervals.begin(), intervals.end());
-        vector<vector<int>> result;
 
+        vector<vector<int>> result;
         result.push_back(intervals[0]);
 
-        for(int i=0; i< intervals.size()-1; i++){
-            if(result[result.size()-1][1] >= intervals[i+1][0] && result[result.size()-1][1] <= intervals[i+1][1]){
-                result[result.size()-1][1] = intervals[i+1][1];
+        for(int i=0; i<intervals.size()-1; i++){
+            if(result[result.size()-1][1] >= intervals[i+1][0]){
+                result[result.size()-1][1] = max(result[result.size()-1][1], intervals[i+1][1]);
             }
-            else if(result[result.size()-1][0] <= intervals[i+1][0] && result[result.size()-1][1] >= intervals[i+1][1]){
-                continue;
-            }
-            else{
-                result.push_back({intervals[i+1][0], intervals[i+1][1]});
-            }
+            else result.push_back(intervals[i+1]);
         }
 
         return result;
