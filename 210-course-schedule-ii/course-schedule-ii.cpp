98c98d27678b16a446bf85a306b74cf15cc96032
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-01-19 23:35:16 -0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (64.59%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    bool dfs(int course, vector<vector<int>>& adj, vector<int>& isExplored, stack<int>& st){
+    bool dfs(int course, vector<vector<int>>& adj, vector<int>& isExplored, vector<int>& result){
         if(isExplored[course]==1) return false; // node is already visited, cycle exists
         if(isExplored[course]==2) return true; // node is already processed
 
         isExplored[course] = 1; // mark visited
         for(int c: adj[course]){
-            if(!dfs(c, adj, isExplored, st)){
+            if(!dfs(c, adj, isExplored, result)){
                 return false;
             }
         }
 
         isExplored[course] = 2; // mark processed
-        st.push(course);
+        result.push_back(course);
 
         return true;
     }
@@ -26,19 +26,14 @@ public:
 
         vector<int> isExplored(numCourses, 0);
         vector<int> result;
-        stack<int> st;
 
         for(int i=0; i < numCourses; i++){
             if(isExplored[i]==0){
-                if(!dfs(i, adj, isExplored, st))
+                if(!dfs(i, adj, isExplored, result))
                     return {};
             }
         }
-        // reverse(result.begin(), result.end());
-        while(!st.empty()){
-            result.push_back(st.top());
-            st.pop();
-        }
+        reverse(result.begin(), result.end());
         return result;
     }
 };
