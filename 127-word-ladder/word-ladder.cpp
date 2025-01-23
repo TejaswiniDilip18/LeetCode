192c2b0814d6eee9053fc477e6f5642bf57d9ab9
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-01-23 01:00:10 -0500

    Time: 350 ms (31.60%) | Memory: 21.2 MB (61.66%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        std::set<string> wordSet;
+
+        // strore wordList in a set
+        for(string st: wordList) wordSet.insert(st);
+
+        std::queue<std::pair<std::string,int>> wordQueue;
+        // int len = beginWord.size();
+
+        int steps = 1;
+
+        wordQueue.push({beginWord, steps});
+
+        while(!wordQueue.empty()){
+            auto [currWord, steps] = wordQueue.front();
+            wordQueue.pop();
+
+            if(currWord == endWord) return steps;
+
+            for(int i=0; i<currWord.size();i++){
+                char tmp = currWord[i];
+                for(char j = 'a'; j <= 'z'; j++){
+                    currWord[i] = j;
+                    if(wordSet.find(currWord) != wordSet.end()){
+                        wordQueue.push({currWord, steps+1});
+                        wordSet.erase(currWord);
+                    }
+                }
+                currWord[i] = tmp;
+            }
+        }
+        return 0;
+    }
+};
