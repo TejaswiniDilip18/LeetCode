64ebc4b3e757e1201c0c315281555d19601dcd75
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-01-24 23:52:11 -0500

    Time: 87 ms (9.26%) | Memory: 31.9 MB (8.43%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -8,33 +8,29 @@ public:
 
         queue<pair<int,int>> neighbors;
 
-        // vector<vector<int>> visited(rows, vector<int>(cols, 0));
+        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
 
         vector<vector<int>> dirs = {{-1,0}, {1,0}, {0,-1}, {0,1}};
 
         for(size_t i=0; i<rows; i++){
             for(size_t j=0; j<cols; j++){
-                if(grid[i][j]=='1'){
+                if(grid[i][j]=='1' && !visited[i][j]){
                     islandCount++;
                     neighbors.push({i,j});
-                    grid[i][j] = '0'; // visited
+                    visited[i][j] = true; // visited
                 
                     while(!neighbors.empty()){
-                        int level = neighbors.size();
+                        auto curr = neighbors.front();
+                        neighbors.pop();
 
-                        for(size_t i=0; i<level; i++){
-                            auto curr = neighbors.front();
-                            neighbors.pop();
+                        for(auto dir: dirs){
+                            int r = curr.first + dir[0];
+                            int c = curr.second + dir[1];
 
-                            for(auto dir: dirs){
-                                int r = curr.first + dir[0];
-                                int c = curr.second + dir[1];
-
-                                if(r>=0 && r<rows && c>=0 && c<cols){
-                                    if(grid[r][c] == '1'){
-                                        grid[r][c] = '0'; // visited
-                                        neighbors.push({r,c});
-                                    }
+                            if(r>=0 && r<rows && c>=0 && c<cols){
+                                if(grid[r][c] == '1' && !visited[r][c]){
+                                    neighbors.push({r,c});
+                                    visited[r][c] = true;
                                 }
                             }
                         }
