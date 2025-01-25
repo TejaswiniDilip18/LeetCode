b0848543e74dc518b8a262e07c9383b073da3e19
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-01-25 00:00:48 -0500

    Time: 4 ms (7.02%) | Memory: 18.8 MB (6.51%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -15,23 +15,23 @@ public:
         vector<vector<int>> dirs = {{-1,0}, {1,0}, {0,-1}, {0,1}};
 
         while(!neighbors.empty()){
-            int level = neighbors.size();
-            for(size_t i=0; i<level; i++){
-                auto curr = neighbors.front();
-                neighbors.pop();
+            // int level = neighbors.size();
+            // for(size_t i=0; i<level; i++){
+            auto curr = neighbors.front();
+            neighbors.pop();
 
-                for(auto dir: dirs){
-                    int newRow = curr.first + dir[0];
-                    int newCol = curr.second + dir[1];
+            for(auto dir: dirs){
+                int newRow = curr.first + dir[0];
+                int newCol = curr.second + dir[1];
 
-                    if(newRow>=0 && newRow<rows && newCol>=0 && newCol<cols){
-                        if(image[newRow][newCol] == startPixel && resultImage[newRow][newCol] != color){
-                            neighbors.push({newRow, newCol});
-                            resultImage[newRow][newCol] = color;
-                        }
+                if(newRow>=0 && newRow<rows && newCol>=0 && newCol<cols){
+                    if(image[newRow][newCol] == startPixel && resultImage[newRow][newCol] != color){
+                        neighbors.push({newRow, newCol});
+                        resultImage[newRow][newCol] = color;
                     }
                 }
             }
+            // }
         }
 
         return resultImage;
