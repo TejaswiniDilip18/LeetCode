a82af882d1a46efeae20cf3234efc9fe98e5cfeb
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-01-29 17:25:57 -0500

    Time: 259 ms (8.43%) | Memory: 85.7 MB (5.05%) - LeetSync

---
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        vector<vector<int>> dirs = {{-1,-1}, {-1,0}, {-1,1},{0,-1},{0,1}, {1, -1}, {1,0},{1,1}}; // 8 dir neighbors
+
+        vector<vector<int>> dist(rows, vector<int>(cols,INT_MAX));
+
+        queue<pair<int, pair<int,int>>> q;
+
+        if(grid[0][0]==0){
+            dist[0][0] = 1;
+            q.push({dist[0][0], {0,0}});
+        }
+        else return -1;
+
+        while(!q.empty()){
+            int currDist = q.front().first;
+            auto [row, col] = q.front().second;
+            q.pop();
+
+            for(auto dir: dirs){
+                int i = row + dir[0];
+                int j = col + dir[1];
+
+                if(i>=0 && i<rows && j>=0 && j<cols && grid[i][j] == 0 && currDist+ 1 < dist[i][j]){
+                    dist[i][j] = currDist + 1;
+                    q.push({dist[i][j], {i,j}});
+                }
+            }
+        }
+
+        return dist[rows-1][cols-1]==INT_MAX ? -1: dist[rows-1][cols-1];
+    }
+};
