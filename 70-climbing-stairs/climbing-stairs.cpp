0c7ecc721c53c2bb3479d4adec464499d3630356
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-02-01 13:28:32 -0500

    Time: 0 ms (100.00%) | Memory: 8.5 MB (50.31%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /* Method 1: Memoization
     int fibonacci(int n, vector<int>& fib){
         if(n<=1){
             return 1;
@@ -12,10 +13,22 @@ public:
         fib[n] = fibonacci(n-1, fib) + fibonacci(n-2, fib);
 
         return fib[n];        
-    }
+    } */
 
     int climbStairs(int n) {
-        vector<int> fib(n+1,-1);
-        return fibonacci(n, fib);
+        // vector<int> fib(n+1,-1); // Method 1: Memoization
+        // return fibonacci(n, fib); // Method 1: Memoization
+
+        vector<int> fib(n+1, 0);
+        fib[1]=1;
+        if(n==1) return 1;
+
+        fib[2] = 2;
+
+        for(int i=3; i<=n ; i++){
+            fib[i] = fib[i-1] + fib[i-2];
+        }  
+
+        return fib[n];      
     }
 };
