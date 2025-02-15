3d12b50bbf0c5d58a37c3a8668e794993bf1628c
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-02-15 01:24:49 -0500

    Time: 15 ms (60.12%) | Memory: 17 MB (65.89%) - LeetSync

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> dist(n, vector<int>(n, INT_MAX));
+        
+        for(auto edge: edges){
+            dist[edge[0]][edge[1]] = edge[2];
+            dist[edge[1]][edge[0]] = edge[2];
+        }
+        
+        for(int i = 0; i< n; i++){
+            dist[i][i] = 0;
+        }
+        
+        for(int mid = 0; mid < n; mid++){
+            for(int i = 0; i< n; i++){
+                for(int j = 0; j< n; j++){
+                    if(dist[i][mid] == INT_MAX || dist[mid][j] == INT_MAX) continue; // INT_MAX means node isn't reachable
+                    
+                    dist[i][j] = min(dist[i][j], (dist[i][mid] + dist[mid][j]));
+                }
+            }
+        }
+        
+        int neighborsMin = n;
+        int city = -1;
+        
+        for(int i = 0; i< n; i++){
+            int count = 0;
+            for(int j = 0; j< n; j++){
+                // for each node at i, check if the dist is less than threshold for all other nodes
+                if(dist[i][j] <= distanceThreshold){
+                    count++;
+                }
+            }
+            if(count != 0 && count < neighborsMin){
+                neighborsMin = count;
+                city = i;
+            }
+            else if(count == neighborsMin){
+                city = max(city, i);
+            }
+        }
+        
+        return city;
+    }
+};
