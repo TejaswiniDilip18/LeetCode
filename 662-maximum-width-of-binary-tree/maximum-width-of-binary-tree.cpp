1464dbdbf3cb34731fd0a008e41135b65e093225
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-02-26 01:20:50 -0500

    Time: 6 ms (4.18%) | Memory: 22.9 MB (5.19%) - LeetSync

---
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        if(!root) return {};
+
+        vector<vector<pair<int,long>>> result;
+        queue<pair<TreeNode*, long>> q;
+
+        q.push({root, 0});
+
+        while(!q.empty()){
+            int level = q.size();
+            vector<pair<int,long>> midResult;
+
+            for(int i=0; i<level; i++){
+                TreeNode* currNode = q.front().first;
+                long idx = q.front().second;
+                q.pop();
+
+                int minIdx;
+                if(i==0) minIdx = idx;
+                idx -= minIdx;
+
+                long leftIdx = (2*idx) + 1;
+                long rightIdx = (2*idx) + 2;
+
+                if(currNode->left) q.push({currNode->left, leftIdx});
+                if(currNode->right) q.push({currNode->right, rightIdx});
+
+                midResult.push_back({currNode->val, idx});
+            }
+            result.push_back(midResult);         
+        }
+
+        int width = INT_MIN;
+
+        for(auto vec: result){
+            if(vec.size()>1){
+                int currWidth = vec[vec.size()-1].second - vec[0].second;
+                width = max(width, currWidth);
+            }
+        }
+
+        return width==INT_MIN? 1: width+1;
+    }
+};
