a0d26674cfce531735c8c61cf2cb4bb9b3f4a614
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-02-26 01:35:04 -0500

    Time: 2 ms (17.84%) | Memory: 19.7 MB (68.76%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -9,49 +9,89 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int widthOfBinaryTree(TreeNode* root) {
+//         if(!root) return {};
+
+//         vector<vector<pair<int,long>>> result;
+//         queue<pair<TreeNode*, long>> q;
+
+//         q.push({root, 0});
+
+//         while(!q.empty()){
+//             int level = q.size();
+//             vector<pair<int,long>> midResult;
+
+//             for(int i=0; i<level; i++){
+//                 TreeNode* currNode = q.front().first;
+//                 long idx = q.front().second;
+//                 q.pop();
+
+//                 int minIdx;
+//                 if(i==0) minIdx = idx;
+//                 idx -= minIdx;
+
+//                 long leftIdx = (2*idx) + 1;
+//                 long rightIdx = (2*idx) + 2;
+
+//                 if(currNode->left) q.push({currNode->left, leftIdx});
+//                 if(currNode->right) q.push({currNode->right, rightIdx});
+
+//                 midResult.push_back({currNode->val, idx});
+//             }
+//             result.push_back(midResult);         
+//         }
+
+//         int width = INT_MIN;
+
+//         for(auto vec: result){
+//             if(vec.size()>1){
+//                 int currWidth = vec[vec.size()-1].second - vec[0].second;
+//                 width = max(width, currWidth);
+//             }
+//         }
+
+//         return width==INT_MIN? 1: width+1;
+//     }
+// };
+
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(!root) return {};
+        if(!root) return 0;
 
-        vector<vector<pair<int,long>>> result;
+        // vector<vector<pair<int,long>>> result;
         queue<pair<TreeNode*, long>> q;
 
         q.push({root, 0});
 
+        int maxWidth = 0;
+
         while(!q.empty()){
             int level = q.size();
             vector<pair<int,long>> midResult;
+            long first = 0, last = 0;
+            long minIdx = q.front().second;
 
             for(int i=0; i<level; i++){
                 TreeNode* currNode = q.front().first;
                 long idx = q.front().second;
                 q.pop();
-
-                int minIdx;
-                if(i==0) minIdx = idx;
                 idx -= minIdx;
 
-                long leftIdx = (2*idx) + 1;
-                long rightIdx = (2*idx) + 2;
+                if(i==0) first = idx;
+                if(i==level-1) last = idx;
+
+                long leftIdx = (2*idx);
+                long rightIdx = (2*idx) + 1;
 
                 if(currNode->left) q.push({currNode->left, leftIdx});
                 if(currNode->right) q.push({currNode->right, rightIdx});
-
-                midResult.push_back({currNode->val, idx});
             }
-            result.push_back(midResult);         
+            maxWidth = max(maxWidth, static_cast<int>(last-first+1));       
         }
 
-        int width = INT_MIN;
-
-        for(auto vec: result){
-            if(vec.size()>1){
-                int currWidth = vec[vec.size()-1].second - vec[0].second;
-                width = max(width, currWidth);
-            }
-        }
-
-        return width==INT_MIN? 1: width+1;
+        return maxWidth;
     }
 };
