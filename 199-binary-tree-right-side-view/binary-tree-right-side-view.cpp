1946e666d4ee443943c0251bcf4895d99853b7ff
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-02-27 19:38:02 -0500

    Time: 0 ms (100.00%) | Memory: 15.1 MB (26.59%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -9,37 +9,63 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root) {
+//         if(!root) return {};
+        
+//         vector<int> result;
+        
+//         map<int, int> mp;
+//         queue<std::pair<TreeNode*, int>> q;
+        
+//         q.push({root,0});
+        
+//         while(!q.empty()){
+//             int level = q.size();
+            
+//             for(int i=0; i<level; i++){
+//                 TreeNode* curr = q.front().first;
+//                 int row = q.front().second;
+//                 q.pop();
+                
+//                 mp[row] = curr->val;
+                
+//                 if(curr->left) q.push({curr->left, row+1});
+//                 if(curr->right) q.push({curr->right, row+1});
+//             }
+//         }
+        
+//         for(auto val: mp){
+//             result.push_back(val.second);
+//         }
+//         return result;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
         if(!root) return {};
         
         vector<int> result;
-        
-        map<int, int> mp;
-        queue<std::pair<TreeNode*, int>> q;
-        
-        q.push({root,0});
+        queue<TreeNode*> q;
+        q.push(root);
         
         while(!q.empty()){
             int level = q.size();
             
             for(int i=0; i<level; i++){
-                TreeNode* curr = q.front().first;
-                int row = q.front().second;
+                TreeNode* curr = q.front();
                 q.pop();
                 
-                // if(mp.find(col) == mp.end()){}
-                mp[row] = curr->val;
+                if(i==level-1) result.push_back(curr->val);
                 
-                if(curr->left) q.push({curr->left, row+1});
-                if(curr->right) q.push({curr->right, row+1});
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
             }
         }
-        
-        for(auto val: mp){
-            result.push_back(val.second);
-        }
+
         return result;
     }
 };
