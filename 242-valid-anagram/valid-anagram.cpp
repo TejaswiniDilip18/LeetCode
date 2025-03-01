8889e3b6d7d8376fb44dd7b6fb3c9b43354d74a5
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-03-01 17:35:04 -0500

    Time: 0 ms (100.00%) | Memory: 9.7 MB (74.09%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,12 +3,25 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()) return false;
 
-        sort(s.begin(),s.end());
-        sort(t.begin(), t.end());
+        // sort(s.begin(),s.end()); // O(nlogn)
+        // sort(t.begin(), t.end());
+
+        // for(int i=0; i<s.size(); i++){       // O(n)
+        //     if(s[i] != t[i]) return false;
+        // }
+        // return true;
+
+        vector<int> freq(26, 0); // frequency of letters a-z
 
         for(int i=0; i<s.size(); i++){
-            if(s[i] != t[i]) return false;
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
         }
+
+        for(int count: freq){
+            if(count != 0) return false;
+        }
+
         return true;
     }
 };
