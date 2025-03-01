12b3b139a121e0c6c819ffb01ccfb0cd8f139c01
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-03-01 17:21:04 -0500

    Time: 12 ms (98.53%) | Memory: 60.9 MB (99.70%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int,int> mp;
-        for(int num: nums){
-            if(mp.find(num) != mp.end()) return true;
-            else mp[num]++;
+        // map<int,int> mp;
+        // for(int num: nums){
+        //     if(mp.find(num) != mp.end()) return true;
+        //     else mp[num]++;
+        // }
+        // return false;
+
+        sort(nums.begin(), nums.end());
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] == nums[i-1]) return true;
         }
         return false;
     }
