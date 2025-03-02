4a39a079d09b51f5259e577c2dc93bf8f4e11fbe
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-03-02 14:17:25 -0500

    Time: 0 ms (100.00%) | Memory: 40 MB (94.23%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,25 +1,45 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> result(nums.size(), 0);
-        int product = 1;
-        int zeroCount = 0;
+// Using division operation
+    // vector<int> productExceptSelf(vector<int>& nums) {
+    //     vector<int> result(nums.size(), 0);
+    //     int product = 1;
+    //     int zeroCount = 0;
 
+    //     for(int i=0; i<nums.size(); i++){
+    //         if(nums[i] == 0){
+    //             zeroCount++;
+    //             continue;
+    //         };
+    //         product = product * nums[i];
+    //     }
+
+    //     if(zeroCount > 1) return result;
+
+    //     for(int i=0; i<nums.size(); i++){
+    //         if(zeroCount == 0) result[i] = product / nums[i];
+    //         else 
+    //             if(nums[i] == 0) result[i] = product;
+    //     }
+    //     return result;
+    // }
+
+// without using division operation
+    vector<int> productExceptSelf(vector<int>& nums){
+        vector<int> result(nums.size(),1);
+        
+        int prefix = 1;
         for(int i=0; i<nums.size(); i++){
-            if(nums[i] == 0){
-                zeroCount++;
-                continue;
-            };
-            product = product * nums[i];
+            result[i] = prefix;
+            prefix = prefix * nums[i];
         }
 
-        if(zeroCount > 1) return result;
-
-        for(int i=0; i<nums.size(); i++){
-            if(zeroCount == 0) result[i] = product / nums[i];
-            else 
-                if(nums[i] == 0) result[i] = product;
+        int suffix = 1;
+        for(int i= nums.size()-1; i>=0; i--){
+            result[i] *= suffix;
+            suffix = suffix * nums[i];
         }
+
         return result;
     }
 };
