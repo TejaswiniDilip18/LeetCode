873b7f7f5b0f039f4482d22b61d54a5a2efc485d
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-03-03 01:13:00 -0500

    Time: 0 ms (100.00%) | Memory: 25.9 MB (80.53%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,25 +1,49 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n = height.size();
+    // int trap(vector<int>& height) {
+    //     int n = height.size();
 
-        vector<int> prefixMax(n);
-        prefixMax[0] = height[0];
-        for(int i=1; i<n; i++){
-            prefixMax[i] = max(prefixMax[i-1], height[i]);
+    //     vector<int> prefixMax(n);
+    //     prefixMax[0] = height[0];
+    //     for(int i=1; i<n; i++){
+    //         prefixMax[i] = max(prefixMax[i-1], height[i]);
+    //     }
+
+    //     vector<int> suffixMax(n);
+    //     suffixMax[n-1] = height[n-1];
+    //     for(int i=n-2; i>=0; i--){
+    //         suffixMax[i] = max(suffixMax[i+1], height[i]);
+    //     }
+
+    //     int trappedWater = 0;
+    //     for(int i=1; i<n-1; i++){
+    //         int currVol = min(prefixMax[i-1], suffixMax[i+1]) - height[i];
+    //         if(currVol > 0) trappedWater += currVol;
+    //     }
+    //     return trappedWater;
+    // }
+
+    // Optimal solution
+    int trap(vector<int>& height){
+        int l = 0;
+        int r = height.size()-1;
+        int leftMax = height[l];
+        int rightMax = height[r];
+        int vol = 0;
+
+        while(l<r){
+            if(height[l] <= height[r]){
+                if(leftMax > height[l]) vol += leftMax - height[l];
+                else leftMax = height[l];
+                l++;
+            }
+            else{
+                if(rightMax > height[r]) vol += rightMax - height[r];
+                else rightMax = height[r];
+                r--;
+            }
         }
 
-        vector<int> suffixMax(n);
-        suffixMax[n-1] = height[n-1];
-        for(int i=n-2; i>=0; i--){
-            suffixMax[i] = max(suffixMax[i+1], height[i]);
-        }
-
-        int trappedWater = 0;
-        for(int i=1; i<n-1; i++){
-            int currVol = min(prefixMax[i-1], suffixMax[i+1]) - height[i];
-            if(currVol > 0) trappedWater += currVol;
-        }
-        return trappedWater;
+        return vol;
     }
 };
