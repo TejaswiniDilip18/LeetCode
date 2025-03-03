d1232ccc137e68ee8712164c1944f3b188acca39
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-03-02 22:12:30 -0500

    Time: 0 ms (100.00%) | Memory: 10 MB (57.48%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string s_new;
-        
-        s.erase(std::remove_if(s.begin(), s.end(), [](unsigned char c) {
-        return std::isspace(c) || !std::isalpha(c) && !std::isdigit(c) ;}), s.end());
-
-        transform(s.begin(), s.end(), s.begin(), [](unsigned char c){return std::tolower(c);});
-
-        // s_new = s;
-        // reverse(s_new.begin(), s_new.end());
-
-        // if(s == s_new) return true;
-        // else return false;
-
-        auto l = s.begin();
-        auto r = s.end() - 1;
+        int l = 0;
+        int r = s.length()-1;
 
         while(l<r){
-            if(*l != *r) return false;
+            while(l<r && !isalnum(s[l])) l++;
+            while(l<r && !isalnum(s[r])) r--;
+            if(tolower(s[l]) != tolower(s[r])) return false;
             l++;
             r--;
         }
