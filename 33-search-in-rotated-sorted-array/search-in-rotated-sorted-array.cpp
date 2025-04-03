d94a9cc85fad15e4ea02244363eec74d2cb0ace2
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-04-02 23:44:52 -0400

    Time: 0 ms (100.00%) | Memory: 15.3 MB (33.70%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -2,27 +2,23 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int left = 0;
-        int right = nums.size()-1;
+        int right = nums.size() - 1;
 
-        // find pivot index
         while(left <= right){
-            int mid = (right+left)/2;
-            
-            if(nums[mid]==target) return mid;
+            int mid = (left+right)/2;
 
-            else if(nums[mid] <= nums[right]){
-                // right half is sorted
-                if(target > nums[mid] && target <= nums[right]){
-                    left = mid+1;
+            if(nums[mid] == target) return mid;
+            else if(nums[left] <= nums[mid]){
+                if(nums[left] <= target && nums[mid] > target){
+                    right = mid - 1;
                 }
-                else right = mid-1;
+                else left = mid + 1;
             }
             else{
-                // left half is sorted
-                if(target < nums[mid] && target >= nums[left]){
-                    right = mid-1;
+                if(nums[right] >= target && nums[mid] < target){
+                    left = mid + 1;
                 }
-                else left = mid+1;
+                else right = mid - 1;
             }
         }
         return -1;
