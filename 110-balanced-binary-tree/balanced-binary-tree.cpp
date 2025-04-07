f056baf68555351bd3809794ee4732ca01415c39
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-04-07 18:33:42 -0400

    Time: 0 ms (100.00%) | Memory: 23 MB (57.90%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,21 +11,22 @@
  */
 class Solution {
 public:
-    int checkDepth(TreeNode* root) {
+    int checkHeight(TreeNode* root){
         if(!root) return 0;
-        int leftHeight = checkDepth(root->left);
-        if(leftHeight == -1) return -1;
 
-        int rightHeight = checkDepth(root->right);
-        if(rightHeight == -1) return -1;
+        int left = checkHeight(root->left);
+        if(left == -1) return -1;
+
+        int right = checkHeight(root->right);
+        if(right==-1) return -1;
+
+        if(abs(left-right) > 1) return -1;
         
-        if(abs(leftHeight - rightHeight) > 1) return -1;
-
-        return max(leftHeight,rightHeight)+1;
+        return 1 + max(left, right);
     }
     bool isBalanced(TreeNode* root) {
-        // if(!root) return true;
-        if(checkDepth(root) == -1) return false;
-        else return true;
+        if(!root) return true;
+
+        return !(checkHeight(root) == -1);
     }
 };
