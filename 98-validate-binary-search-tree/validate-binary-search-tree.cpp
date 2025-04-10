6a291315e09c6a4e4c42eb910a01c99cfe67ca1f
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-04-09 22:55:30 -0400

    Time: 3 ms (6.38%) | Memory: 21.7 MB (94.97%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -14,9 +14,7 @@ public:
     bool checkBST(TreeNode* root, long minVal, long maxVal){
         if(!root) return true;
         if(root->val <= minVal || root->val >= maxVal) return false;
-
-        return (checkBST(root->left, minVal, static_cast<long>(root->val)) && 
-                checkBST(root->right, static_cast<long>(root->val), maxVal));
+        return checkBST(root->left, minVal, root->val) && checkBST(root->right, root->val, maxVal);
     }
     bool isValidBST(TreeNode* root) {
         return checkBST(root, LONG_MIN, LONG_MAX);
