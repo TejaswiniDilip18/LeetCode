930c0f5bd204e7d0218f717759c9908351d18163
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-04-19 14:59:17 -0400

    Time: 0 ms (100.00%) | Memory: 71.7 MB (53.43%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -5,9 +5,9 @@ public:
         int currSum = 0;
 
         for(int& n: nums){
-            if(currSum < 0) currSum=0;
             currSum += n;
             maxSum = max(maxSum, currSum);
+            if(currSum < 0) currSum=0;
         }
         return maxSum;
     }
