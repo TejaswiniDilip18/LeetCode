8585f58e8de2ef5e2e3d9de41d1b6c9921ce5c8e
Tejaswini Dilip Deore <126366115+TejaswiniDilip18@users.noreply.github.com>
2025-07-03 23:49:22 -0400

    Time: 15 ms (42.05%) | Memory: 18 MB (32.71%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,14 +3,21 @@ class Solution:
         if(len(s) != len(t)):
             return False
         
-        counts = [0]*26
+        # counts = [0]*26
         
+        # for i in range(len(s)):
+        #     counts[ord(s[i]) - ord('a')] += 1
+        #     counts[ord(t[i]) - ord('a')] -= 1
+        
+        # for count in counts:
+        #     if(count != 0):
+        #         return False
+
+        s_dict = {}
+        t_dict = {}
+
         for i in range(len(s)):
-            counts[ord(s[i]) - ord('a')] += 1
-            counts[ord(t[i]) - ord('a')] -= 1
+            s_dict[s[i]] = 1 + s_dict.get(s[i], 0)
+            t_dict[t[i]] = 1 + t_dict.get(t[i], 0)
         
-        for count in counts:
-            if(count != 0):
-                return False
-        
-        return True
+        return s_dict == t_dict
